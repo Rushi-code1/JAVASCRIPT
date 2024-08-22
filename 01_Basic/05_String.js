515b08fdd8c61c80bd0e156543c1898e46316290
Rushikesh Sunil Deshmukh <mictosoft789@gmail.com>
2024-08-22 10:16:13 +0000

    add String file

---
+++ 01_Basic/05_String.js
@@ -0,0 +1,31 @@
+const name = "Rushikesh";
+const repoNumber = 50;
+console.log(name + repoNumber);
+
+console.log(`My name is ${name} and github repo count is ${repoNumber}`);
+
+const GameName = new String("Hello world");
+console.log(GameName)
+
+// console.log(GameName[1])
+// console.log(GameName.length)
+// console.log(GameName.toUpperCase())
+// console.log(GameName.toLocaleLowerCase())
+
+
+
+// console.log(GameName.__proto__);
+
+// console.log(GameName.charAt(2))
+// console.log(GameName.indexOf("e"))
+
+// const newDtring = GameName.substring(0,5)
+// console.log(newDtring)
+
+// const newString= GameName.slice(-11,-1)
+// console.log(newString)
+
+const Stringsplit = GameName.split(" ")
+console.log(Stringsplit);
+
+
