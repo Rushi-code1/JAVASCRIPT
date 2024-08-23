1355b0973cc5b6c9c13aa4519e1de548552618d4
Rushikesh Sunil Deshmukh <mictosoft789@gmail.com>
2024-08-23 04:41:06 +0000

    comit

---
+++ 01_Basic/06_numberAndmath.js
@@ -0,0 +1,38 @@
+const score = 400
+console.log(score);
+
+const balance = new Number(100);
+console.log(balance)
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(1));
+
+const otherNumber = 23.896776543
+
+console.log(otherNumber.toPrecision(3))
+
+const otherNumber1 = 123.896776543
+
+console.log(otherNumber1.toPrecision(3))
+
+const hundreds = 10000000
+console.log(hundreds.toLocaleString('en-IN'))
+
+
+const isNAN = "10"
+
+console.log(isNAN.isNAN)
+
+//  +++++++++++++++++++++ Maths ++++++++++++++++++++++++++++++
+console.log(Math.exp(2,3))
+console.log(Math.pow(2,4))
+console.log(Math.PI);
+console.log(Math.abs(-4));
+console.log(Math.abs(4));
+
+console.log(Math.max(12,2,3,4,23,34,50,4,45,39,53));
+console.log(Math.min(12,2,3,4,23,34,50,4,45,39,53));
+console.log(Math.tanh)
+
+
+
